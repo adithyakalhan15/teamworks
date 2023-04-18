210857476cd9449a3b70d420802d749f1a7b59af
adithyakalhan15 <adithyakalhan15@gmail.com>
2023-04-18 19:15:48 +0530

    edit

---
+++ design/usj_pub_front/src/pages/register/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Navlog from "react-router-dom/components/navbar/navlog";
+
+export default function Register(){
+    return(
+        <>
+        <Head>
+            <title>Register</title>
+        </Head>
+        <Navlog/>
+        <main>
+            
+        </main>
+        </>
+    )
+}
