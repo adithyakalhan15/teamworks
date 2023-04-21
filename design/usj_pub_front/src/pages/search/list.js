ac7a4124ff3d5e02d12ebaed434b58b185284347
adithyakalhan15 <adithyakalhan15@gmail.com>
2023-04-21 15:45:45 +0530

    update

---
+++ design/usj_pub_front/src/pages/search/list.js
@@ -0,0 +1,35 @@
+import image1 from '/public/pub_imgs/listimg1.png';
+
+var title, disp, auth, date, tags, img="";
+
+const resulting = [
+    {
+        id: 1,
+        title: "Main title of the publication.",
+        img: '/pub_imgs/listimg1.png',
+        auth: 'author1',
+        disp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis odio ut metus sodales interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan elementum nisl eu aliquam. Proin at nulla imperdiet, auctor lacus ac, viverra ante. In sit amet purus at diam mattis.",
+        date: "2022",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    },
+    {
+        id: 2,
+        title: "Main title of the publication.",
+        img: '/pub_imgs/listimg1.png',
+        auth: 'author1',
+        disp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis odio ut metus sodales interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan elementum nisl eu aliquam. Proin at nulla imperdiet, auctor lacus ac, viverra ante. In sit amet purus at diam mattis.",
+        date: "2021",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    },
+    {
+        id: 2,
+        title: "Main title of the publication.",
+        img: '/pub_imgs/listimg1.png',
+        auth: 'author1',
+        disp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis odio ut metus sodales interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan elementum nisl eu aliquam. Proin at nulla imperdiet, auctor lacus ac, viverra ante. In sit amet purus at diam mattis.",
+        date: "2021",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    }
+]
+
+export default resulting;
